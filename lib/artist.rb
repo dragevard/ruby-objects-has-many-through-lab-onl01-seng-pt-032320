c56d9fed51fbd0058a88448333ff6d732d7f98e8
Derek phung <dragevards@gmail.com>
2020-05-06 17:02:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist 
+  
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << name 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_song(name, genre)
+    new_song = Song.new(name, genre)
+    new_song.artist = self 
+  end 
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist = self 
+    end 
+  end 
+  
+  def genres 
+    Song.all.map do |song|
+      song.genre 
+    end 
+  end 
+    
+end

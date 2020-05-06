b50c533412e2f55036d98d5b715b21e69ab9f833
Derek phung <dragevards@gmail.com>
2020-05-06 17:30:28 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end 
   
   def new_appointment(date, doctor)
-    new_appointment = Appointment.new(date, doctor, self)
+    new_appointment = Appointment.new(date, self, doctor)
   end 
   
   def appointments 
